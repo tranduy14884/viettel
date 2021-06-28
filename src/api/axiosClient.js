fa1c1d0b72689b9d56e4d218449076b3f2dc729f
Duy Tran <tranduy14884@gmail.com>
2021-06-28 17:40:17 +0700

    complete

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-    baseURL : 'http://localhost:3000/api',
+    baseURL : 'https://server-viettelcantho-demo.herokuapp.com/api',
     headers : {
         'Content-Type' : 'application/json',
     }
